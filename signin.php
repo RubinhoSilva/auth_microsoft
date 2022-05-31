2a83a72ee3cfae70f626638a10a5ae0f42264ea6
RubinhoSilva <rubinho142@gmail.com>
2022-05-31 20:02:47 -0300

    🚀 debug in prod

--- signin.php
+++ signin.php
@@ -6,18 +6,17 @@ ini_set('display_errors', 'on');
 require "vendor/autoload.php";
 use myPHPnotes\Microsoft\Auth;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+//$dotenv->load();
 
 $tenant = "common";
-//var_dump($_ENV['CLIENT_ID']);
-////
-////$client_id = $_ENV['CLIENT_ID'];
-////$client_secret = $_ENV['CLIENTE_SECRET'];
-////$callback = $_ENV['CALLBACK'];
-////$scopes = ["User.Read"];
-////
-////
-////$microsoft = new Auth($tenant, $client_id, $client_secret,$callback, $scopes);
-////
-////header("location: " . $microsoft->getAuthUrl());
+
+$client_id = getenv('CLIENT_ID');
+$client_secret = $_ENV['CLIENTE_SECRET'];
+$callback = $_ENV['CALLBACK'];
+$scopes = ["User.Read"];
+
+
+$microsoft = new Auth($tenant, $client_id, $client_secret,$callback, $scopes);
+
+header("location: " . $microsoft->getAuthUrl());
