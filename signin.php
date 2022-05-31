08bcf0a9a7cffc79e9361fb59e6288330d066dcc
RubinhoSilva <rubinho142@gmail.com>
2022-05-31 19:59:06 -0300

    🚀 debug in prod

--- signin.php
+++ signin.php
@@ -9,13 +9,13 @@ $dotenv->load();
 
 $tenant = "common";
 var_dump($_ENV['CLIENT_ID']);
-
-$client_id = $_ENV['CLIENT_ID'];
-$client_secret = $_ENV['CLIENTE_SECRET'];
-$callback = $_ENV['CALLBACK'];
-$scopes = ["User.Read"];
-
-
-$microsoft = new Auth($tenant, $client_id, $client_secret,$callback, $scopes);
-
-header("location: " . $microsoft->getAuthUrl());
+//
+//$client_id = $_ENV['CLIENT_ID'];
+//$client_secret = $_ENV['CLIENTE_SECRET'];
+//$callback = $_ENV['CALLBACK'];
+//$scopes = ["User.Read"];
+//
+//
+//$microsoft = new Auth($tenant, $client_id, $client_secret,$callback, $scopes);
+//
+//header("location: " . $microsoft->getAuthUrl());
